34f407604a9000023060773970a9b67494cd8a49
Trent Lowe <trentrlowe@gmail.com>
2023-04-18 13:47:38 -0400

    Update laurel spacing

--- frontend/pages/project/[slug].js
+++ frontend/pages/project/[slug].js
@@ -55,11 +55,11 @@ const Project = ({ project, about = [] }) => {
           {project?.laurels && (
             <Field>
               <Label>Recognition</Label>
-              <div className="flex flex-wrap gap-xs">
+              <div className="flex flex-wrap gap-xs items-center">
                 {project?.laurels.map((image) => (
                   <Image
                     key={image._key}
-                    className="w-[calc((1/4*100%)-9px)] md:w-[calc((1/6*100%)-10px)] lg:w-[calc((1/8*100%)-11px)] xl:w-[calc((1/6*100%)-10px)] h-auto"
+                    className="w-[calc((1/4*100%)-10px)] md:w-[calc((1/6*100%)-11px)] lg:w-[calc((1/8*100%)-12px)] xl:w-[calc((1/6*100%)-11px)] h-full"
                     src={urlFor(image).url()}
                     alt="Laurel"
                     width={100}
